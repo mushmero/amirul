fc97c934d068c07940a47a801541b1581c2b27e2
amirul <hakim_s09@hotmail.com>
2019-05-10 21:34:58 +0800

    update website address

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
-<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
-<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
 <head>
     <title>Resume of Muhd Amirul Hakim Zailan</title>
     <!-- Meta -->
@@ -9,22 +9,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Resume of Muhd Amirul Hakim Zailan.">
-    <meta name="author" content="Amirul Hakim">    
-    <link rel="shortcut icon" href="favicon.ico">  
+    <meta name="author" content="Amirul Hakim">
+    <link rel="shortcut icon" href="favicon.ico">
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
     <!-- Global CSS -->
-    <link type="text/css" rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <link type="text/css" rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
     <!-- Plugins CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    
-    <!-- Theme CSS -->  
+
+    <!-- Theme CSS -->
     <link type="text/css" id="theme-style" rel="stylesheet" href="assets/css/styles.css">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-</head> 
+</head>
 
 <body>
     <div class="wrapper">
@@ -34,12 +34,12 @@
                 <h1 class="name">Amirul Hakim</h1>
                 <h3 class="tagline">Postgraduate Student</h3>
             </div><!--//profile-container-->
-            
+
             <div class="contact-container container-block">
                 <ul class="list-unstyled contact-list">
                     <li class="email"><i class="fa fa-envelope"></i><a href="mailto: amirulhakimzailan@gmail.com">amirulhakimzailan@gmail.com</a></li>
                     <li class="phone"><i class="fa fa-phone"></i><a href="tel:+60136715075">+60 13 671 5075</a></li>
-                    <li class="website"><i class="fa fa-globe"></i><a href="http://mushmero.com" target="_blank">mushmero.com</a></li>
+                    <li class="website"><i class="fa fa-globe"></i><a href="https://mushmero.herokuapp.com" target="_blank">mushmero.herokuapp.com</a></li>
                     <li class="linkedin"><i class="fa fa-linkedin"></i><a href="https://www.linkedin.com/in/muhdamirulhakim/" target="_blank">muhdamirulhakim</a></li>
                     <li class="github"><i class="fa fa-github"></i><a href="https://github.com/mushmero" target="_blank">@mushmero</a></li>
                     <li class="gitlab"><i class="fa fa-gitlab"></i><a href="https://gitlab.com/mushmero" target="_blank">@mushmero</a></li>
@@ -60,7 +60,7 @@
                     <div class="time">2011 - 2015</div>
                 </div><!--//item-->
             </div><!--//education-container-->
-            
+
             <div class="languages-container container-block">
                 <h2 class="container-block-title">Languages</h2>
                 <ul class="list-unstyled interests-list">
@@ -68,7 +68,7 @@
                     <li>English <span class="lang-desc">(Professional)</span></li>
                 </ul>
             </div><!--//interests-->
-            
+
             <div class="interests-container container-block">
                 <h2 class="container-block-title">Interests</h2>
                 <ul class="list-unstyled interests-list">
@@ -78,21 +78,21 @@
                     <li>Explore new things</li>
                 </ul>
             </div><!--//interests-->
-            
+
         </div><!--//sidebar-wrapper-->
-        
+
         <div class="main-wrapper">
-            
+
             <section class="section summary-section">
                 <h2 class="section-title"><i class="fa fa-user"></i>Objective</h2>
                 <div class="summary">
                     <p align="justify">The purpose of writing this resume is to acquire the suitable job in information & technology field with experiences & skills. I am graduated from Universiti Putra Malaysia (UPM) in B. Computer Science in 2015 and continue my study on database research in 2016 in M. Sc. Database System doing research on data storage size for an IoT application. Since 2015 I have been developing some mini projects regarding web applications using Codeigniter, setup DO VPS, using Git for my code repo and etc. </p>
                 </div><!--//summary-->
             </section><!--//section-->
-            
+
             <section class="section experiences-section">
                 <h2 class="section-title"><i class="fa fa-briefcase"></i>Experiences</h2>
-                
+
                 <div class="item">
                     <div class="meta">
                         <div class="upper-row">
@@ -105,7 +105,7 @@
                         <p>Assist existing development team in developing a system that manage teacher to apply position for school headmaster or school principal using inhouse php framework and oracle DB. The system will be use by entire national public school teachers and also staff on state education department.</p>
                     </div><!--//details-->
                 </div><!--//item-->
-                
+
                 <div class="item">
                     <div class="meta">
                         <div class="upper-row">
@@ -115,11 +115,11 @@
                         <div class="company">HRA Solutions, Seri Kembangan</div>
                     </div><!--//meta-->
                     <div class="details">
-                        <p>Assist and support IT staff on CitiBank Tower as an support technician by helping them migrating and transferring old PC to new PC as well as implementing software backup and restore.</p>  
-                        
+                        <p>Assist and support IT staff on CitiBank Tower as an support technician by helping them migrating and transferring old PC to new PC as well as implementing software backup and restore.</p>
+
                     </div><!--//details-->
                 </div><!--//item-->
-                
+
                 <div class="item">
                     <div class="meta">
                         <div class="upper-row">
@@ -129,12 +129,12 @@
                         <div class="company">Xentral Methods, Cyberjaya</div>
                     </div><!--//meta-->
                     <div class="details">
-                        <p>As an internship student, helping them convert conventional format ebook which is (PDF) into ebook format (EPUB). Propose an idea by having multiple checkbox for their main site.</p>  
+                        <p>As an internship student, helping them convert conventional format ebook which is (PDF) into ebook format (EPUB). Propose an idea by having multiple checkbox for their main site.</p>
                     </div><!--//details-->
                 </div><!--//item-->
-                
+
             </section><!--//section-->
-            
+
             <section class="section projects-section">
                 <h2 class="section-title"><i class="fa fa-archive"></i>Projects</h2>
                 <div class="intro">
@@ -142,10 +142,10 @@
                 </div><!--//intro-->
                 <div class="item">
                     <span class="project-title"><a href="http://demo-proseller.mushmero.com" target="_blank">Proseller</a></span> - <span class="project-tagline">A project that assist a standalone online seller to manage their inventory and financial flow. Develop by using CodeIgniter framework, Bootstrap framework, AdminLTE css, dompdf and chartjs</span>
-                    
+
                 </div><!--//item-->
                 <div class="item">
-                    <span class="project-title"><a href="#" target="_blank">Attendance-Pro</a><small> in progress</small></span> - 
+                    <span class="project-title"><a href="#" target="_blank">Attendance-Pro</a><small> in progress</small></span> -
                     <span class="project-tagline">A simple system that will be use to track staff attendance on a company and generate report based on month or department. Develop by using CodeIgniter framework, Bootstrap framework, AdminLTE css and dompdf.</span>
                 </div><!--//item-->
                 <div class="item">
@@ -155,56 +155,56 @@
                     <span class="project-title"><a href="#" target="_blank">Humidity, Temperature & UV sensor</a><small> in progress</small></span> - <span class="project-tagline">An IoT project that will read humidity, temperature and UV reading from environment and transmit the data into online database over wireless network. Develop by using Arduino board, UV sensors, DHT sensors, ESP8266 Wifi Module.</span>
                 </div><!--//item-->
             </section><!--//section-->
-            
+
             <section class="skills-section section">
                 <h2 class="section-title"><i class="fa fa-rocket"></i>Skills &amp; Proficiency</h2>
-                <div class="skillset">        
+                <div class="skillset">
                     <div class="item">
                         <h3 class="level-title">PHP</h3>
                         <div class="level-bar">
                             <div class="level-bar-inner" data-level="85%">
-                            </div>                                      
-                        </div><!--//level-bar-->                                 
+                            </div>
+                        </div><!--//level-bar-->
                     </div><!--//item-->
-                    
+
                     <div class="item">
                         <h3 class="level-title">HTML5 &amp; CSS</h3>
                         <div class="level-bar">
                             <div class="level-bar-inner" data-level="85%">
-                            </div>                                      
-                        </div><!--//level-bar-->                                 
+                            </div>
+                        </div><!--//level-bar-->
                     </div><!--//item-->
-                    
+
                     <div class="item">
                         <h3 class="level-title">Javascript &amp; jQuery</h3>
                         <div class="level-bar">
                             <div class="level-bar-inner" data-level="20%">
-                            </div>                                      
-                        </div><!--//level-bar-->                                 
+                            </div>
+                        </div><!--//level-bar-->
                     </div><!--//item-->
-                    
+
                     <div class="item">
                         <h3 class="level-title">Arduino C</h3>
                         <div class="level-bar">
                             <div class="level-bar-inner" data-level="60%">
-                            </div>                                      
-                        </div><!--//level-bar-->                                 
+                            </div>
+                        </div><!--//level-bar-->
                     </div><!--//item-->
 
                     <div class="item">
                         <h3 class="level-title">DigitalOcean Cloud Server</h3>
                         <div class="level-bar">
                             <div class="level-bar-inner" data-level="55%">
-                            </div>                                      
-                        </div><!--//level-bar-->                                 
+                            </div>
+                        </div><!--//level-bar-->
                     </div><!--//item-->
-                    
-                </div>  
+
+                </div>
             </section><!--//skills-section-->
-            
+
         </div><!--//main-body-->
     </div>
- 
+
     <footer class="footer">
         <div class="text-center">
                 <p><i class="fa fa-print"></i> Print or download this Resume as PDF <a href="to-pdf.php"><strong>here</strong></a></p>
@@ -212,12 +212,12 @@
                 <small class="copyright">Designed with <i class="fa fa-heart"></i> by <a href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> for developers. Edited by <a href="https://mushmero.com" target="_blank">myself</a></small>
         </div><!--//container-->
     </footer><!--//footer-->
- 
-    <!-- Javascript -->          
+
+    <!-- Javascript -->
     <script type="text/javascript" src="assets/plugins/jquery-1.11.3.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>    
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
     <!-- custom js -->
-    <script type="text/javascript" src="assets/js/main.js"></script>            
+    <script type="text/javascript" src="assets/js/main.js"></script>
 </body>
-</html> 
+</html>
 
